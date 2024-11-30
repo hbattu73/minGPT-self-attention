f84fca561ff3c71dd8deb0c847f3ca62367d1396
Hemanth Battu <hbattu73@gmail.com>
2024-11-29 19:02:28 -0800

    Perceiver code

--- src/submission/model.py
+++ src/submission/model.py
@@ -87,6 +87,19 @@ class DownProjectBlock(nn.Module):
         ### Hint: Copy over the code from Block and make necessary modifications.
 
         ### START CODE HERE
+        self.ln1 = nn.LayerNorm(config.n_embd)
+        self.ln2 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalCrossAttention(config)
+        self.mlp = nn.Sequential(
+            nn.Linear(config.n_embd, 4 * config.n_embd),
+            nn.GELU(),
+            nn.Linear(4 * config.n_embd, config.n_embd),
+            nn.Dropout(config.resid_pdrop),
+        )
+        self.C = nn.Parameter(
+            torch.empty(1, config.bottleneck_dim, config.n_embd)  
+        )
+        nn.init.xavier_uniform_(self.C) 
         ### END CODE HERE
 
     def forward(self, x_input):
@@ -98,6 +111,9 @@ class DownProjectBlock(nn.Module):
         ### Should be around 3-5 lines.
 
         ### START CODE HERE
+        x = x_input + self.attn(self.ln1(x), self.ln1(self.C))
+        x = x + self.mlp(self.ln2(x))
+        return x
         ### END CODE HERE
 
 
@@ -114,6 +130,15 @@ class UpProjectBlock(nn.Module):
         ### Hint: Copy over the code from Block and make necessary modifications.
 
         ### START CODE HERE
+        self.ln1 = nn.LayerNorm(config.n_embd)
+        self.ln2 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalCrossAttention(config)
+        self.mlp = nn.Sequential(
+            nn.Linear(config.n_embd, 4 * config.n_embd),
+            nn.GELU(),
+            nn.Linear(4 * config.n_embd, config.n_embd),
+            nn.Dropout(config.resid_pdrop),
+        )
         ### END CODE HERE
 
     def forward(self, y, x_input):
@@ -126,6 +151,9 @@ class UpProjectBlock(nn.Module):
         ### Should be around 3-5 lines.
 
         ### START CODE HERE
+        y = y + self.attn(self.ln1(y), self.ln1(x_input))
+        y = y + self.mlp(self.ln2(y))
+        return y
         ### END CODE HERE
 
 class GPT(nn.Module):
